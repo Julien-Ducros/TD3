705fc949d9d3305ab3ed874a650ea08308d7b0b5
Julien <ducrosjulien.pro@gmail.com>
2020-04-08 11:47:46 +0200

    + TD4

--- app/src/main/java/com/example/td3/MainActivity.java
+++ app/src/main/java/com/example/td3/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.td3;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -9,18 +16,31 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+
+import com.example.td3.R;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.td3.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -31,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       showlist();
+       makeApiCall();
+    }
+
+    private void showlist() {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -46,9 +71,9 @@ public class MainActivity extends AppCompatActivity {
             input.add("Test" + i);
         }// define an adapter
 
-        mAdapter = new ListAdapter(input);
+        mAdapter= new com.example.td3.ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
-       ;
+        ;
     }
 
     @Override
@@ -72,4 +97,46 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+    
+    private void makeApiCall() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        PokeAPI pokeAPI = retrofit.create(PokeAPI.class);
+
+        Call<RestPokemonResponse> call = pokeAPI.getPokemonResponse();
+        call.enqueue(new Callback<RestPokemonResponse>() {
+            @Override
+            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+
+                    List<Pokemon> pokemonList = response.body().getResults();
+                    Toast.makeText(getApplicationContext(),"API Succes",Toast.LENGTH_SHORT).show();
+
+                } else {
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                showError();
+            }
+        });
+    }
+
+    private void showError() {
+            Toast.makeText(getApplicationContext(),"API ERROR",Toast.LENGTH_SHORT).show();
+    }
+
+    }
+
+
+
